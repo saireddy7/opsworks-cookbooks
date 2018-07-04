9206e28dc5b68750a115346a48c78cbcb3305cc2
testuser <charvee@example.com>
2018-07-04 16:51:49 +0530

    changes

--- charvee_test/recipes/aws.rb
+++ charvee_test/recipes/aws.rb
@@ -1,12 +1,12 @@
 
 puts 'install awscli'
 
-#package 'python3' do 
-#	command "pip install awscli"
-#	action :rung
-#end
+include_recipe 'cloud_cli_aws::s3_bucket'
 
-python 'awscli' do
-  action :install
- 
+aws_s3_bucket 'charvee_test' do
+  aws_access_key aws node[:awscli][:aws_access_key_id]
+  aws_secret_access_key aws node[:awscli][:aws_secret_access_key]
+  versioning true
+  region 'us-west-1'
+  action :create
 end
