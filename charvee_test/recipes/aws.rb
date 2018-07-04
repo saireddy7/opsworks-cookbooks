5c2210668534cca7319c9627a12a668296b6d5ee
testuser <charvee@example.com>
2018-07-04 15:50:45 +0530

    changes

--- charvee_test/recipes/aws.rb
+++ charvee_test/recipes/aws.rb
@@ -1,7 +1,12 @@
 
 puts 'install awscli'
 
-package 'python3' do 
-	command "pip install awscli"
-	action :rung
+#package 'python3' do 
+#	command "pip install awscli"
+#	action :rung
+#end
+
+python_package 'awscli' do
+  action :install
+ 
 end
