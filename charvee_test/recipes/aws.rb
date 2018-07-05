ae202f3a294367085bbd9662c7b71f2f0e2fd02c
testuser <charvee@example.com>
2018-07-05 11:33:34 +0530

    changes

--- charvee_test/recipes/aws.rb
+++ charvee_test/recipes/aws.rb
@@ -1,7 +1,10 @@
 
 puts 'install awscli'
 
-include_resource 'cloud_cli_aws_s3_bucket'
+include_ 'cloud_cli_aws_s3_bucket'
+
+Chef::Recipe.send(:include, s3_bucket)
+Chef::Resource::create.send(:include, s3_bucket)
 
 aws_s3_bucket 'charvee_test' do
   aws_access_key aws node[:awscli][:aws_access_key_id]
