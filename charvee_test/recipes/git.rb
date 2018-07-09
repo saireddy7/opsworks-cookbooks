d0614fcea884eb4daaf1baafec8979a3b9fce0bd
testuser <charvee@example.com>
2018-07-09 16:56:17 +0530

    git

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -2,8 +2,8 @@ puts 'charvee git example'
 
 include_recipe 'git'
 
-git "var/www/html/" do
-  repository "ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit"
-  reference "master"
-  action :sync
-end
+#git "var/www/html/" do
+#  repository "ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit"
+#  reference "master"
+#  action :sync
+#end
