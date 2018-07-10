213a91c1d95865f7d3f6855b699e02c33c0c9013
testuser <charvee@example.com>
2018-07-10 13:52:47 +0530

    changes

--- charvee_test/recipes/aws.rb
+++ charvee_test/recipes/aws.rb
@@ -17,16 +17,19 @@
 
 include_recipe 'aws'
 
-aws_s3_file "var/www/html/" do
-  bucket "codepipeline-us-east-1-51835399533"
-  remote_path "/testpipeline/MyAppBuild"
-  aws_access_key_id node[:awscli][:aws_access_key_id]
-  aws_secret_access_key node[:awscli][:aws_secret_access_key]
-end
+#aws_s3_file "var/www/html/" do
+#  bucket "codepipeline-us-east-1-51835399533"
+#  remote_path "/testpipeline/MyAppBuild"
+#  aws_access_key_id node[:awscli][:aws_access_key_id]
+#  aws_secret_access_key node[:awscli][:aws_secret_access_key]
+#end
 
 execute "clone" do
+   aws_access_key_id node[:awscli][:aws_access_key_id]
+   aws_secret_access_key node[:awscli][:aws_secret_access_key]
    command 'echo "[credential] \nhelper = !aws --profile CodeCommitProfile codecommit credential-helper $@ \nUseHttpPath = true" > ~/.gitconfig'
    command 'git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
+
 #  repository "ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit"
 #  reference "master"
 #  action :sync
