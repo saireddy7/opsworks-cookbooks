752e8e8cf57ee63333bd0e41f074c8be578cafaa
testuser <charvee@example.com>
2018-07-10 14:07:25 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -10,8 +10,9 @@ puts 'charvee git example'
 
 
 execute "var/www/html/" do
-   command 'echo "[credential] \nhelper = !aws --profile CodeCommitProfile codecommit credential-helper $@ \nUseHttpPath = true" > ~/.gitconfig'
-   command 'git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
+   command 'apt-get update; apt-get install git-core; git --version'
+ #  command 'echo "[credential] \nhelper = !aws --profile CodeCommitProfile codecommit credential-helper $@ \nUseHttpPath = true" > ~/.gitconfig'
+ #  command 'git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
 #  repository "ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit"
 #  reference "master"
 #  action :sync
