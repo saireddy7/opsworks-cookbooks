955daad9c9e3f89c9b2802b22ac9ca8bbc98e4c4
testuser <charvee@example.com>
2018-07-11 11:36:45 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -11,6 +11,7 @@
 
 execute 'test' do
 	echo "hello"
+	command 'git clone https://node[:awscli][:GIT_USER]:node[:awscli][:GIT_PASS]@git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
 #command 'vi etc/test.txt'
  #  code 'echo "[credential] \nhelper = !aws --profile CodeCommitProfile codecommit credential-helper $@ \nUseHttpPath = true \n\n[user] \nemail = charvee@example.com \nname = testuser" > ~/.gitconfig'
   # command 'git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
