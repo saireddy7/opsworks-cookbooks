aa063f4ac0834838472c5e2f003a8ef2020567df
testuser <charvee@example.com>
2018-07-12 20:54:04 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -15,9 +15,11 @@ execute 'test' do
 	#command 'git clone https://'+node[:awscli][:GIT_USER]+':'+node[:awscli][:GIT_PASS]+'@git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit; git describe --abbrev=0 --tags'
 
 	#command 'myapp-codecommit'
-	command `git describe --abbrev=0 --tags`
+	
 
-#	cwd 'myapp-codecommit/myapp-codecommit'
+	cwd 'myapp-codecommit/myapp-codecommit'
+
+	command `git describe --abbrev=0 --tags`
     #command 'ls -latr'
 #  # command 'git describe --abbrev=0 --tags'
 	#output = `git describe --abbrev=0 --tags`
