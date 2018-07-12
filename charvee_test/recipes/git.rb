ba37046ecd4aaf05fdcfded95de807efaf7c1d5e
testuser <charvee@example.com>
2018-07-12 16:13:14 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -20,7 +20,7 @@ execute 'test' do
 	cwd 'myapp-codecommit/myapp-codecommit'
     #command 'ls -latr'
    # command '$(git describe --abbrev=0 --tags)'
-    command = shell_out(“git describe --abbrev=0 --tags”)
+    command = 'shell_out(git describe --abbrev=0 --tags)'
 	#output = `git describe --abbrev=0 --tags`
 	#commond 'ls -latr'
 	puts "output is #{command}"
