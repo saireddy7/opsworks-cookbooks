4b0cf9d03d5446fed5c26d404c541c29b7c61cd5
testuser <charvee@example.com>
2018-07-13 16:13:44 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -14,7 +14,9 @@ execute 'test' do
 	#command 'echo -e "protocol=https\npath=/v1/repos/myapp-codecommit\nhost=git-codecommit.us-east-1.amazonaws.com" | aws codecommit credential-helper get'
 #	command 'git clone https://'+node[:awscli][:GIT_USER]+':'+node[:awscli][:GIT_PASS]+'@git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
     cwd 'myapp-codecommit/myfiles'
-    command = 'git describe --abbrev=0 --tags'
+   # command 'git describe --abbrev=0 --tags'
+    command 'last_tag=$(git describe --abbrev=0 --tags)'
+    out 'echo $last_tag'
   #  command  `mkdir charvee`
   #  command  'mkdir punia'
 #    command `cat pom.xml`
@@ -22,7 +24,7 @@ execute 'test' do
 	#command = `ls -latr`
 	#command =`mkdir charvee`
 	#command = `git describe --abbrev=0 --tags`
-puts "output is #{command}"
+puts "output is #{out}"
 	
 
 #	command "git describe --abbrev=0 --tags"
