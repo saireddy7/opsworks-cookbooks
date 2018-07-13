ad4c23a384d85b39df096aeae814bdc7b22b6f35
testuser <charvee@example.com>
2018-07-13 15:07:20 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -15,7 +15,7 @@ execute 'test' do
 	#command 'git clone https://'+node[:awscli][:GIT_USER]+':'+node[:awscli][:GIT_PASS]+'@git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
 
 	#command 'myapp-codecommit'
-	cwd '/home/myapp-codecommit/myapp-codecommit'
+	cwd '/root/myapp-codecommit/myapp-codecommit'
 	#command = `ls -latr`
 	#command =`mkdir charvee`
 	command = `git describe --abbrev=0 --tags`
