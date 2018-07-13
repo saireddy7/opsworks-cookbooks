29fb95f153af40c82b717d8a86192922a1bf1910
testuser <charvee@example.com>
2018-07-13 11:55:53 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -19,8 +19,8 @@ execute 'test' do
 
 	cwd 'myapp-codecommit/myapp-codecommit'
 
-	command `git describe --abbrev=0 --tags`
-    #command 'ls -latr'
+	#command `git describe --abbrev=0 --tags`
+    command 'ls -latr'
 #  # command 'git describe --abbrev=0 --tags'
 	#output = `git describe --abbrev=0 --tags`
 	#commond 'ls -latr'
