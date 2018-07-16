ed8cd566cd04685d2084c3b797540836afc89ffc
testuser <charvee@example.com>
2018-07-16 13:15:02 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -18,10 +18,10 @@
 #end
 
 
-remote_file "master-1.0.177.jar" do
+remote_file "#{git_describe}" do
   source lazy {
     git_describe = shell_out!('git describe --abbrev=0 --tags', cwd: 'myapp-codecommit/myfiles').stdout.strip
-     "https://s3.amazonaws.com/versiontags/master-1.0.177.jar"
+     "https://s3.amazonaws.com/versiontags/#{git_describe}"
   }
 end
 
