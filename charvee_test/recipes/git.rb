71b3cb5fa0be0493f50cd91033c579d50d43a159
testuser <charvee@example.com>
2018-08-29 14:20:46 +0530

    changes

--- charvee_test/recipes/git.rb
+++ charvee_test/recipes/git.rb
@@ -2,7 +2,7 @@
 
 execute 'test' do
  #   cwd 'myapp-codecommit/myfiles'
-  command 'git clone https://'+node[:awscli][:GIT_USER]+':'+node[:awscli][:GIT_PASS]+'charvee@bitbucket.org/charvee/node-jenkins-pipeline-test.git'
+  command 'git clone https://'+node[:awscli][:GIT_USER]+':'+node[:awscli][:GIT_PASS]+'@bitbucket.org/charvee/node-jenkins-pipeline-test.git'
     #command 'git clone https://'+node[:awscli][:GIT_USER]+':'+node[:awscli][:GIT_PASS]+'@git-codecommit.us-east-1.amazonaws.com/v1/repos/myapp-codecommit'
  # command '$(git describe --abbrev=0 --tags)'
   
